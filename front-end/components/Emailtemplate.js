89b608cc31b127ba8dc1a32c1eef4479157c674f
Mohanaprasath <mohanaprasath12345@gmail.com>
2021-11-21 03:25:44 -0500

    sidebar fixed

--- front-end/components/Emailtemplate.js
+++ front-end/components/Emailtemplate.js
@@ -1,10 +1,7 @@
 
 import React, { useState } from 'react'
-import Head from "next/head";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { flexbox, height } from '@mui/system';
+
 import styles from '../styles/emailtemplate.module.css'
-import Layout from "../components/Layout";
 import {
     Table,
     TableBody,
@@ -13,11 +10,10 @@ import {
     TableHead,
     TableRow,
     Toolbar,
-    Link,
-    Button
   } from "@mui/material";
 
 export default function Emailtemplate() {
+  var values="Default";
     const[addmail,setaddmail]=useState(false);
   const[showtitle,setshowtitle]=useState(true)
 function handlepopup(){
@@ -28,10 +24,6 @@ function handlepopup(){
         <div>
             <div className={styles.page}>
       
-       
-      <Head>
-      <title>Email Templates | Settings</title>
-    </Head>
    
  
       
@@ -40,28 +32,30 @@ function handlepopup(){
        ( 
        <div style={{display:"flex",justifyContent:"space-between"}}> 
        <h2>Email Templates</h2>
-        <button onClick={()=>handlepopup()} class="btn btn-primary">+New Email Templates</button> 
+        <button onClick={()=>handlepopup()} className={styles.addmaillbtn}>+New Email Templates</button> 
         </div>
        )
         : <div></div>}
 
       <hr/>
      {addmail ? (
-      <div className={styles.popup} id="popup-1">
+      <div className={styles.popup}>
         <div className={styles.overlay}></div>
         <div className={styles.content}>
        <div className={styles.form}>
          <form style={{height:"auto",width:"100%",justifyContent:"center",alignItems:"center"}}>
             <div style={{display:"flex",justifyContent:"space-between"}}> 
-  <h4>New Email Template</h4>
-  <svg onClick={()=>setaddmail(false)} xmlns="http:www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <h4>New Email Template</h4><br/>
+  <svg onClick={()=>{setaddmail(false);setshowtitle(true)}} 
+  xmlns="http:www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
+ viewBox="0 0 16 16">
  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg>
-</div>
- <div class="form-group d-flex">
-   <label for="exampleFormControlSelect1">Email Type</label>
-   <select class="form-control" id="exampleFormControlSelect1">
+</div><br/><br/>
+ <div>
+   <label>Email Type</label>
+   <select >
    <option></option>
      <option>Trip Notification</option>
      <option>Report Notification</option>
@@ -71,23 +65,23 @@ function handlepopup(){
    </select>
  </div>
  <br/>
- <div class="form-group d-flex">
-   <label for="formGroupExampleInput">Template Type</label>
-   <input value="Default" type="text" class="form-control" id="formGroupExampleInput"></input>
+ <div>
+   <label>Template Type</label>
+   <input initialValue="Default" type="text"></input>
  </div>
  <br/>
- <div class="form-group d-flex">
-   <label for="formGroupExampleInput">Subject</label>
-   <input type="text" class="form-control" id="formGroupExampleInput"></input>
+ <div>
+   <label >Subject</label>
+   <input type="text"></input>
  </div>
  <br/><br/>
- <div class="form-group">
-   <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+ <div>
+   <textarea rows="5" style={{width:"100%"}}></textarea>
  </div>
  <br/>
- <div class="d-flex">
-   <button class="btn btn-primary">SAVE</button>
-   <button onClick={()=>{setaddmail(false);setshowtitle(true)}} class="btn btn-light">CANCEL</button>
+ <div>
+   <button>SAVE</button>
+   <button onClick={()=>{setaddmail(false);setshowtitle(true)}}>CANCEL</button>
  </div>
 </form>
 </div>
@@ -97,18 +91,21 @@ function handlepopup(){
      <div></div>
      }
 
-     <div class="card">
+     <div>
 
- <div class="card-body">
-   <h5 class="card-title" style={{color:"orange"}}>
-       <svg style={{color:"white",background:"orange"}} xmlns="http:www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-question-square" viewBox="0 0 16 16">
+ <div >
+   <br/>
+   <h5 style={{color:"orange"}}>
+       <svg style={{color:"white",background:"orange"}} 
+       xmlns="http:www.w3.org/2000/svg" width="20" height="20" fill="currentColor" 
+      viewBox="0 0 16 16">
  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
  <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
 </svg>
-       PAGE TIPS</h5>
-   <h2 class="card-text" style={{fontWeight:"100",fontSize:"30px"}}>Customize email alerts</h2>
+       PAGE TIPS</h5><br/>
+   <h2 style={{fontWeight:"100",fontSize:"30px"}}>Customize email alerts</h2><br/>
   <p>Configure templates for emails automatically sent when inviting users to the 
-      organization and when associated<br/> workflows are triggered.<a href="#">Learn More.</a></p>
+      organization and when associated<br/> workflows are triggered.<a href="#" style={{color:"blue"}}>Learn More.</a></p>
 </div>
 </div>
 
@@ -117,14 +114,10 @@ function handlepopup(){
 
        <Toolbar />
          <TableContainer>
-      
-          
            <Table>
              <TableHead>
                <TableRow>
-                
                  <TableCell>NAME</TableCell>
-                
                  <TableCell>TYPE</TableCell>
                  <TableCell>SUBJECT AND CONTENT</TableCell>
                </TableRow>
@@ -142,3 +135,4 @@ function handlepopup(){
         </div>
     )
 }
+
--- front-end/pages/emailtemplate.js
+++ front-end/pages/emailtemplate.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 import Layout from "../components/Layout";
-import Emailtemplate from '../components/Emailtemplate';
-
+import Emailtemplate from '../components/Emailtemplate'
+import Head from "next/head";
 
 export default function emailtemplate() {
     return (
-      <>
+      <div>
       <Layout />
       <Emailtemplate/>
-</>
+      <Head>
+        <title>Email Template | Settings</title>
+        <script
+          src="https://kit.fontawesome.com/1794b9b2a9.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      </div>
     )
 }
