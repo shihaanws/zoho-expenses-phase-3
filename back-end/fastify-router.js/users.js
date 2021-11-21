907816f3b2caa3e35cd346d0fe4f44b5993704a2
saranya <saranyac2727@gmail.com>
2021-11-21 13:27:20 +0530

    user err fixed

--- back-end/fastify-router.js/users.js
+++ back-end/fastify-router.js/users.js
@@ -1,24 +1,24 @@
-// const usermodule = require("../controllers/users");
-// module.exports = async function (fastify, opts) {
-//   fastify.get(
-//     "/api/getallusers/:orgname",
-//     {
-//       preValidation: [fastify.jwtauth],
-//     },
-//     usermodule.getAllUser
-//   ),
-//     fastify.get(
-//       "/api/getuser/:id",
-//       {
-//         preValidation: [fastify.jwtauth],
-//       },
-//       usermodule.getUserById
-//     ),
-//     fastify.put(
-//       "/api/updateuser/:id",
-//       {
-//         preValidation: [fastify.jwtauth],
-//       },
-//       usermodule.updateUserById
-//     );
-// };
+const usermodule = require("../controllers/users");
+module.exports = async function (fastify, opts) {
+  fastify.get(
+    "/api/getallusers/:orgname",
+    {
+      preValidation: [fastify.jwtauth],
+    },
+    usermodule.getAllUser
+  ),
+    fastify.get(
+      "/api/getuser/:id",
+      {
+        preValidation: [fastify.jwtauth],
+      },
+      usermodule.getUserById
+    ),
+    fastify.put(
+      "/api/updateuser/:id",
+      {
+        preValidation: [fastify.jwtauth],
+      },
+      usermodule.updateUserById
+    );
+};
